95bce1edd59d2786dfb8a871a95a7d5bd18d76b1
Luisa Scavo <luisascavo@gmail.com>
2018-03-24 11:39:12 -0400

    add actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,27 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+    
+    def new
+    end
+
+    def create
+    end
+
+    def show
+
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+    def set_book
+        @book = Book.find(params[:id])
+    end
 end
