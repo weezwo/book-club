3aeb0c05e4891eaf2aaf9771996898505fbd40dc
Luisa Scavo <luisascavo@gmail.com>
2018-03-24 11:40:45 -0400

    initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c = Club.create(name: "Ypsi Readers")
+c.books.create(title: "Catcher in the Rye", author: "J.D. Salinger")
