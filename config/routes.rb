5fd7812327a09adb45708e5ba523ea28a2fb59e7
Luisa Scavo <luisascavo@gmail.com>
2018-03-24 09:48:59 -0400

    initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  resources :clubs do
+    resources :books, only: [:index, :new, :create]
+  end
+
+  resources :books, only: [:show, :edit, :update, :destroy] do
+    resources :thoughts
+  end
+
+
 end
