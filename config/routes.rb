25709c4c42021631b4c2900d6e9f581a8eb2b8eb
Luisa Scavo <luisascavo@gmail.com>
2018-06-15 21:43:03 -0400

    Add login routes, update page route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static#home'
   
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   resources :users
 
   resources :clubs do
@@ -10,6 +13,9 @@ Rails.application.routes.draw do
 
   resources :books, only: [:show, :edit, :update, :destroy] do
     resources :thoughts
+    member do
+      patch 'update_page'
+    end
   end
 
 
