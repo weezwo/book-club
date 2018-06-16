64c63079805a6712cce0f68b2e287da7105a7f67
Luisa Scavo <luisascavo@gmail.com>
2018-06-16 09:53:27 -0400

    initial create action

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,18 @@
 class RepliesController < ApplicationController
+    def create
+        @thought = Thought.find(params[:thought_id])
+        @reply = @thought.replies.build(reply_params)
+        @reply.user = current_user
+        if @reply.save
+            redirect_to book_thought_path(@thought.book, @thought) 
+        else
+            raise @reply.errors.inspect
+            render :'/thoughts/show'    
+        end
+    end
+
+    private
+    def reply_params
+        params.require(:reply).permit(:content, :spoiler, :associated_page)    
+    end
 end
