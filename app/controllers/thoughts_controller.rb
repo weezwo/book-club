77d5e8074ee41816c282bb8f38f02b9604a63f94
Luisa Scavo <luisascavo@gmail.com>
2018-06-15 21:45:15 -0400

    initial thoughts controller

--- app/controllers/thoughts_controller.rb
+++ app/controllers/thoughts_controller.rb
@@ -1,2 +1,22 @@
 class ThoughtsController < ApplicationController
+
+    def create
+        @thought = current_user.thoughts.new(thought_params)
+        @thought.book = Book.find(params[:book_id])
+        if @thought.save
+            flash[:message] = "SUCCESS!!"
+        else
+            flash[:message] = "Nah; #{@thought.errors.full_messages.join(" and ")}."
+        end
+        redirect_to "/books/#{params[:book_id]}"
+    end
+
+    def show
+        raise params.inspect
+    end
+
+    private
+    def thought_params
+        params.require(:thought).permit(:title, :content, :associated_page)
+    end
 end
