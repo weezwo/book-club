15b49815fde6bd9edfe3108b00a4a51ab53ca412
Luisa Scavo <luisascavo@gmail.com>
2018-06-15 21:42:42 -0400

    add password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180324141245) do
+ActiveRecord::Schema.define(version: 20180615194944) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20180324141245) do
     t.text "profile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
