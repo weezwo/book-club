b8594b877ad7d4ed3ab9d117d254fd87a3f670ea
Luisa Scavo <luisascavo@gmail.com>
2018-08-30 17:48:50 -0400

    add sessions#destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,9 @@ class SessionsController < ApplicationController
             redirect_to login_path
         end
     end
+
+    def destroy
+        session.destroy
+        redirect_to root_path
+    end
 end
