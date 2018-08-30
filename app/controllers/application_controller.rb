de87590bb7117ae1dcd1292477e9527f0c91a77d
Luisa Scavo <luisascavo@gmail.com>
2018-08-30 17:48:38 -0400

    add redirect methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_user
 
+  def redirect_if_not_logged_in
+    unless logged_in?
+      redirect_to root_path
+    end
+  end
+  
+  def redirect_if_logged_in
+    if logged_in?
+      redirect_to root_path
+    end
+  end
+
   def logged_in?
     !!current_user
   end
